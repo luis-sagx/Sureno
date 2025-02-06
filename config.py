21d10ffef06b1a3717d3a62cc29f13fbb731d26c
Luis Eduardo Sagnay Pilamunga <sagnayluis99@gmail.com>
2025-02-05 22:35:36 -0500

    base de datos

---
+++ config.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient 
+import os
+
+class Config:
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://lsagnay:Ur9EcG84r3GJNegi@cluster0.ljtbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+client = MongoClient(Config.MONGO_URI)
+
+db = client.Sureno  
